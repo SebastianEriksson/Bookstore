26c90abf67fb2bd77a7e88e55fb4278a52981e27
Sebastian Eriksson <Sebastian Eriksson>
2016-10-10 04:56:44 +0300

    Second commit

--- src/test/java/fi/haagahelia/course/ExerciseApplicationTests.java
+++ src/test/java/fi/haagahelia/course/ExerciseApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExerciseThreeApplicationTests {
+public class ExerciseApplicationTests {
 
 	@Test
 	public void contextLoads() {

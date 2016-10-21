8f3c5522740eed0f02bb4530c5e769dfe494ec62
Sebastian Eriksson <Sebastian Eriksson>
2016-10-22 02:23:32 +0300

    Third commit. Create entity for db + create repo along with some pre-made books. Add a basic bootstrap css and create add + delete buttons. Also create everything else needed for a simple db of books.

--- src/main/java/fi/haagahelia/course/BookListApplication.java
+++ src/main/java/fi/haagahelia/course/BookListApplication.java
@@ -1,12 +1,38 @@
 package fi.haagahelia.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
 
 @SpringBootApplication
-public class ExerciseApplication {
+public class BookListApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookListApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExerciseApplication.class, args);
+		SpringApplication.run(BookListApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			
+			log.info("Add a couple of book to the database");
+			repository.save(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 
+					1995, "0345391802", 7.19));
+			repository.save(new Book("Java For Dummies", "Barry A. Burd",
+					2014, "1118407806", 23.73));
+			
+			log.info("show all the books in the database");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 }
--- src/test/java/fi/haagahelia/course/BookListApplicationTests.java
+++ src/test/java/fi/haagahelia/course/BookListApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExerciseApplicationTests {
+public class BookListApplicationTests {
 
 	@Test
 	public void contextLoads() {

5ea4380fab82dcbae6f7ee2ad1f348cb2efca3fd
SebastianEriksson <erikssonsebastian@outlook.com>
2016-10-22 03:27:42 +0300

    Fourth commit. Should now link to my github account correctly. Added categories to the project. Minor formatting corrections.

--- src/main/java/fi/haagahelia/course/BookListApplication.java
+++ src/main/java/fi/haagahelia/course/BookListApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.BookRepository;
+import fi.haagahelia.course.domain.Category;
+import fi.haagahelia.course.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookListApplication {
@@ -20,14 +22,35 @@ public class BookListApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
-			
 			log.info("Add a couple of book to the database");
+			
+			crepository.save(new Category("Science fiction"));
+			crepository.save(new Category("Satire"));
+			crepository.save(new Category("Drama"));
+			crepository.save(new Category("Action and Adventure"));
+			crepository.save(new Category("Romance"));
+			crepository.save(new Category("Mystery"));
+			crepository.save(new Category("Horror"));
+			crepository.save(new Category("Self help"));
+			crepository.save(new Category("Health"));
+			crepository.save(new Category("Guide"));
+			crepository.save(new Category("Science"));
+			crepository.save(new Category("Children's"));
+			crepository.save(new Category("History"));
+			crepository.save(new Category("Poetry"));
+			crepository.save(new Category("Comics"));
+			crepository.save(new Category("Encyclopedias"));
+			crepository.save(new Category("Fantasy"));
+			crepository.save(new Category("Autobiographies"));
+			crepository.save(new Category("Journals"));
+			
+			
 			repository.save(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 
-					1995, "0345391802", 7.19));
+					1995, "0345391802", 7.19, crepository.findByName("Science fiction").get(0)));
 			repository.save(new Book("Java For Dummies", "Barry A. Burd",
-					2014, "1118407806", 23.73));
+					2014, "1118407806", 23.73, crepository.findByName("Science").get(0)));
 			
 			log.info("show all the books in the database");
 			for (Book book : repository.findAll()) {
--- src/main/java/fi/haagahelia/course/domain/Book.java
+++ src/main/java/fi/haagahelia/course/domain/Book.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -16,14 +18,19 @@ public class Book {
 	private String isbn;
 	private double price;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
+	
 	public Book(){}
 	
-	public Book(String title, String author, int year, String isbn, double price) {
+	public Book(String title, String author, int year, String isbn, double price, Category category) {
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 	
 	public Long getId() {
@@ -63,10 +70,22 @@ public class Book {
 		this.price = price;
 	}
 	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
 	@Override
 	public String toString() {
-		return "Book id=" + id + ", title=" + title + ", author=" + author +
-				", year=" + year + ", ISBN=" + isbn + ", price=" + price;
+		if (this.category != null)
+			return "Book [id=" + id + ", title=" + title + ", author=" + author +
+				", year=" + year + ", ISBN=" + isbn + ", price=" + price + 
+				", category =" + this.getCategory() + "]";
+		else
+			return "Book [id=" + id + ", title=" + title + ", author=" + author +
+				", year=" + year + ", ISBN=" + isbn + ", price=" + price + "]";
 	}
 
 }

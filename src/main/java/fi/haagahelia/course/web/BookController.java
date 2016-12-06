9060247f6b4e52fb5c929be7abd8406785da1cbc
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-06 19:10:15 +0200

    Add import

--- src/main/java/fi/haagahelia/course/web/BookController.java
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -3,6 +3,7 @@ package fi.haagahelia.course.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;

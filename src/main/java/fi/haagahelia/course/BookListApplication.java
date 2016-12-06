4af4db840d0c3fa1bccc95899bf4328dedf03266
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-06 19:07:31 +0200

    Add premade user and admin with hashed passwords
    - User = password
    - Admin = admin

--- src/main/java/fi/haagahelia/course/BookListApplication.java
+++ src/main/java/fi/haagahelia/course/BookListApplication.java
@@ -12,6 +12,8 @@ import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.BookRepository;
 import fi.haagahelia.course.domain.Category;
 import fi.haagahelia.course.domain.CategoryRepository;
+import fi.haagahelia.course.domain.User;
+import fi.haagahelia.course.domain.UserRepository;
 
 @SpringBootApplication
 public class BookListApplication {
@@ -22,7 +24,7 @@ public class BookListApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("Add a couple of book to the database");
 			
@@ -46,11 +48,19 @@ public class BookListApplication {
 			crepository.save(new Category("Autobiographies"));
 			crepository.save(new Category("Journals"));
 			
-			
+			// Create pre-made test books
 			repository.save(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 
 					1995, "0345391802", 7.19, crepository.findByName("Science fiction").get(0)));
 			repository.save(new Book("Java For Dummies", "Barry A. Burd",
 					2014, "1118407806", 23.73, crepository.findByName("Science").get(0)));
+			
+			// Create pre-made test users
+			User user1 = new User("user", 
+					"$2a$10$pw6Z8aFUI6T94OauF08U7ubXlKK9yFubDj1jubtn4QuH3z1Epf8Hu", "USER");
+			User user2 = new User("admin", 
+					"$2a$10$GZ6VDMDq6LuOS.DyCeOxzujWCGGnkPq7nMwkpvLgysf3.hI81wQg.", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("show all the books in the database");
 			for (Book book : repository.findAll()) {

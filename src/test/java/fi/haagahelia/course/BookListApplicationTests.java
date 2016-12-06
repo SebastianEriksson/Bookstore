b8717f3c429c2f91d732c595cf341bfa07eabdad
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-06 20:31:46 +0200

    Begin final checks
    - Import Spring Boot Testing dep
    - Create Application Test classes
    - Test JPA
    - Test Web
    - Fix stuff in pom.xml
    - overall fixes to code

--- src/test/java/fi/haagahelia/course/BookListApplicationTests.java
+++ src/test/java/fi/haagahelia/course/BookListApplicationTests.java
@@ -1,16 +1,35 @@
 package fi.haagahelia.course;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import fi.haagahelia.course.web.BookController;
+import fi.haagahelia.course.web.UserController;
+
+/**
+ * 
+ * Testing that the context is creating your controller
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookListApplicationTests {
-
+	
+	@Autowired
+	private BookController controller;
+	
+	@Autowired
+	private UserController controller2;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+		assertThat(controller2).isNotNull();
 	}
 
 }
---
+++ src/test/java/fi/haagahelia/course/JpaDataTest.java
@@ -0,0 +1,35 @@
+package fi.haagahelia.course;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
+import fi.haagahelia.course.domain.CategoryRepository;
+import fi.haagahelia.course.domain.UserRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class JpaDataTest {
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository repository2;
+	
+	@Autowired UserRepository repository3;
+	
+	@Test
+	public void findByTitleShouldReturnBook() {
+		List<Book> books = repository.findByTitle("");
+		assertThat(books).hasSize(1);
+	}
+}
